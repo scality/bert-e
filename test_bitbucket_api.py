2b29297ba4cb2e5d324f13f1c95841b0218a7bab
mvaude <maxime.vaude@scality.com>
2015-12-24 15:47:00 +0100

    add empty_line

--- test_bitbucket_api.py
+++ test_bitbucket_api.py
@@ -4,6 +4,7 @@
 import unittest
 from bitbucket_api import (get_bitbucket_client,
                            Repository as BitBucketRepository)
+
 from git_api import Repository as GitRepository
 
 
