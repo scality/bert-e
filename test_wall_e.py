56e03df8a3e6d7d14a0835201064b467f5aabc55
Rayene Ben Rayana <rayene.benrayana@scality.com>
2015-12-29 14:28:57 +0100

    fixes tests

--- test_wall_e.py
+++ test_wall_e.py
@@ -205,14 +205,14 @@ class TestWallE(unittest.TestCase):
                                             bypass_jira_version_check=True,
                                             bypass_jira_type_check=True)
 
-    def set_build_status(self, issue_id, state):
+    def set_build_status(self, issue_id, key, state):
         dst_branch = 'bugfix/RING-%s' % issue_id
         pr = self.create_feature_branch_and_pull_request(dst_branch,
                                                          'development/4.3')
         self.bbrepo.set_build_status(state=state,
                                      url='http://example.com',
                                      revision=pr['source']['commit']['hash'],
-                                     key='jenkins_utest')
+                                     key=key)
         self.wall_e.handle_pull_request('scality', self.bbrepo['repo_slug'],
                                         pr['id'],
                                         bypass_jira_version_check=True,
@@ -220,11 +220,11 @@ class TestWallE(unittest.TestCase):
 
     def test_build_status_fail(self):
         with self.assertRaises(BuildFailedException):
-            self.set_build_status('0008', 'FAILED')
+            self.set_build_status('0008', 'jenkins_build', 'FAILED')
 
     def test_build_status_inprogress(self):
         with self.assertRaises(BuildInProgressException):
-            self.set_build_status('0009', 'INPROGRESS')
+            self.set_build_status('0009', 'jenkins_build', 'INPROGRESS')
 
     # FIXME: Find a way to test the successful state
     # def test_build_status_success(self):
