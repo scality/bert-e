c10217c4dac62f8b2193c328583396c9ea35616b
Sylvain Killian <sylvain.killian@scality.com>
2016-01-08 16:05:04 +0100

    hide cmd stdout/stderr from standard logging level

--- simplecmd.py
+++ simplecmd.py
@@ -3,10 +3,19 @@
 
 import subprocess
 import logging
+import os
 
 
 def cmd(_):
-    logging.debug('')
-    logging.debug('#' * 50)
-    logging.debug('# BASH : %s' % _)
-    subprocess.check_call(_, shell=True)
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
+        logging.debug('')
+        logging.debug('#' * 50)
+        logging.debug('# BASH : %s' % _)
+
+        stdout_ = None
+        stderr_ = None
+    else:
+        stdout_ = open(os.devnull, 'wb')
+        stderr_ = open(os.devnull, 'wb')
+
+    subprocess.check_call(_, shell=True, stdout=stdout_, stderr=stderr_)
