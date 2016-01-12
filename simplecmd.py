3673daba72c7c8d8f4721c7219ccac668421240b
Pierre-Louis Bonicoli <pierrelouis.bonicoli@scality.com>
2016-01-12 19:59:32 +0100

    cmd: allow to pass kwargs
    Issue: RELENG-338

--- simplecmd.py
+++ simplecmd.py
@@ -6,13 +6,14 @@ import logging
 import os
 
 
-def cmd(_):
+def cmd(_, shell=True, stderr=None, **kwargs):
     if logging.getLogger().isEnabledFor(logging.DEBUG):
         logging.debug('')
         logging.debug('#' * 50)
         logging.debug('# BASH : %s' % _)
 
-        return subprocess.check_output(_, shell=True)
+        return subprocess.check_output(_, shell=shell, stderr=stderr, **kwargs)
     else:
         with open(os.devnull, 'wb') as devnull:
-            return subprocess.check_output(_, shell=True, stderr=devnull)
+            return subprocess.check_output(_, shell=shell, stderr=devnull,
+                                           **kwargs)
