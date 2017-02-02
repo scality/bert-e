af80606a4d4f639412e98b530a6ceade6f7ad2ff
Geoffrey ROYER <geoffrey.royer@scality.com>
2017-02-02 15:10:59 +0100

    Fix typo in git_host/__init__.py

--- bert_e/git_host/__init__.py
+++ bert_e/git_host/__init__.py
@@ -1,7 +1,7 @@
 """
 Git host module
 """
-# flake8: noqa : this import initializes the factory
+# flake8: noqa: this import initializes the factory
 from .base import Error, RepositoryExists, NoSuchRepository, NoSuchGitHost
 from .factory import client_factory
 from . import bitbucket, mock
