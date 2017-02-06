082ca9ea80380ac37604ae6eb6846afdcfc2a4b9
Arnaud Calmettes <calmettes.arnaud@gmail.com>
2017-02-06 11:26:40 +0100

    BERTE-260 Add Reactor class + unit tests

---
+++ bert_e/reactor.py
@@ -0,0 +1,348 @@
+# Copyright 2016 Scality
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module implements an independent command reactor.
+
+The reactor is intended to hold the definition and implementation of commands
+that the user might want BertE to react to while parsing a pull request's
+comments.
+
+We distinguish two classes of "commands":
+
+    * Command: Perform an immediate action of any kind.
+    * Option: Set an option for the whole lifetime of a pull request.
+
+Commands have the following attributes:
+
+    * handler: a function that takes a job as its first argument (and an
+      arbitrarily long list of arguments).
+
+    * help: a string intended to be displayed when the user requests the
+      command's manual.
+
+    * privileged: a boolean indicating if the command is to be performed
+      by privileged users only.
+
+Options have an additional `default` attribute that describes the option's
+default value.
+
+The Reactor class implemented in this module is intended to be plugin-friendly.
+If one wants to add new options or commands to the reactor, all he has to do
+is register them using the corresponding class methods.
+
+Examples:
+
+    # Register an unprivileged command to the 'say_hi' key, using the
+    # function's docstring as the command's help.
+    @Reactor.command
+    def say_hi(job, *args):
+        '''Print 'hi' in the console.'''
+        print("hi")
+
+    # Register a privileged command to the 'shutdown' key.
+    @Reactor.command(privileged=True)
+    def shutdown(job, *args):
+        '''Shut the application down.'''
+        raise SystemExit(0)
+
+    # Register a privileged command using custom help string.
+    @Reactor.command(privileged=True, help='Execute a shell command.')
+    def shell(job, *args):
+        '''Run a command in the shell.'''
+        subprocess.call(args)
+
+    # Register a boolean option that will be set in job.settings to `True` when
+    # called by a privileged user. Default value is `False`
+    Reactor.add_option('bypass_checks', privileged=True)
+
+    # Register an unprivileged option with special behavior.
+    @Reactor.option(default=set())
+    def after_pull_request(job, *pr_ids):
+        job.settings.after_pull_request.update(pr_ids)
+
+
+    #To use the Reactor, instanciate it.
+    reactor = Reactor()
+
+    job = SimpleNamespace(settings={})
+
+    # This will print 'hi' in the console
+    reactor.handle_commands(job, '!do say_hi', prefix='!do')
+
+    # This will raise a NotPrivileged error
+    reactor.handle_commands(job, '!do shell ls', prefix='!do')
+
+    # This will execute the 'ls' command in a shell
+    reactor.handle_commands(job, '!do shell ls', prefix='!do', privileged=True)
+
+"""
+
+# Please note that special effort was made to keep this module relatively
+# independent from the rest of the application.
+# Its only non-standard dependencies are the Dispatcher utility mixin,
+# and the fact that a 'job' must have a 'settings' dictionary-like attribute.
+
+import logging
+import re
+from collections import namedtuple
+from copy import copy
+
+from .utils import Dispatcher
+
+
+class Error(Exception):
+    """Base class for errors raised in this module."""
+    pass
+
+
+class NotPrivileged(Error):
+    """A non-privileged user tried to used a privileged command or option."""
+    def __init__(self, keyword: str):
+        super().__init__()
+        self.keyword = keyword
+
+
+class NotFound(Error):
+    """The requested command or option doesn't exist."""
+    def __init__(self, keyword: str):
+        super().__init__()
+        self.keyword = keyword
+
+
+LOG = logging.getLogger(__name__)
+
+Command = namedtuple('Command', ['handler', 'help', 'privileged'])
+Option = namedtuple('Option', ['handler', 'default', 'help', 'privileged'])
+
+
+def normalize_whitespace(msg):
+    """Sanitize help message by removing extra white space.
+
+    This helps keeping clean help messages when passed through a function's
+    docstring.
+
+    """
+    if msg is None:
+        return msg
+    return ' '.join(msg.strip().split())
+
+
+class Reactor(Dispatcher):
+    """Central dispatching class for comment-based commands and options."""
+
+    @classmethod
+    def add_command(cls, key, handler, help_=None, privileged=False):
+        """Register a new command to the reactor."""
+        help_ = normalize_whitespace(help_ or handler.__doc__)
+        cls.set_callback(key, Command(handler, help_, privileged))
+
+    @classmethod
+    def command(cls, key=None, help_=None, privileged=False):
+        """Decorator to register a command.
+
+        Args:
+            key: the key to register the command to. Defaults to the
+                 decorated function's name.
+            help_: the help message of the command. Defaults to the
+                   decorated function's docstring.
+            privileged: whether the command is privileged or not. Defaults to
+                        False.
+        """
+
+        if callable(key):
+            # the decorator was called with the @Reactor.command syntax
+            func = key
+            cls.add_command(func.__name__, func, func.__doc__, False)
+            return func
+
+        # the decorator was called with the @Reactor.command(...) syntax
+        def decorator(func):
+            _key = key or func.__name__
+            _help = help_ or func.__doc__
+            cls.add_command(_key, func, _help, privileged)
+            return func
+
+        return decorator
+
+    @classmethod
+    def add_option(cls, key, help_=None, privileged=False, default=None):
+        """Add a basic option to the reactor."""
+
+        def set_option(job, arg=True):
+            job.settings[key] = arg
+
+        help_ = normalize_whitespace(help_)
+        cls.set_callback(key, Option(set_option, default, help_, privileged))
+
+    @classmethod
+    def option(cls, key=None, default=None, help_=None, privileged=False):
+        """Decorator to register an option handler.
+
+        Args:
+            default: the setting's default value. Defaults to None.
+
+        See Reactor.commands() for detail on other args.
+
+        """
+
+        if callable(key):
+            # The decorator was called with the @Reactor.option syntax
+            func = key
+            help_ = normalize_whitespace(help_ or func.__doc__)
+            cls.set_callback(
+                func.__name__, Option(func, default, help_, privileged)
+            )
+            return func
+
+        # the decorator was called with the @Reactor.option(...) syntax
+        def decorator(func):
+            _key = key or func.__name__
+            _help = normalize_whitespace(help_ or func.__doc__)
+            cls.set_callback(_key, Option(func, default, _help, privileged))
+            return func
+        return decorator
+
+    @classmethod
+    def get_options(cls):
+        """Return a dictionary depicting currently registered options."""
+        return {key: val for key, val in cls.__callbacks__.items()
+                if isinstance(val, Option)}
+
+    @classmethod
+    def get_commands(cls):
+        """Return a dictionary depicting currently registered commands."""
+        return {key: val for key, val in cls.__callbacks__.items()
+                if isinstance(val, Command)}
+
+    def init_settings(self, job):
+        """Initialize a job's settings to the registered options' default
+        values.
+
+        """
+        for key, option in self.get_options().items():
+            job.settings[key] = copy(option.default)
+
+    def handle_options(self, job, text, prefix, privileged=False):
+        """Find option calls in given text string, and execute the
+        corresponding option handlers if any is found.
+
+        An option declaration can be on the following forms:
+
+            {prefix}option1=val1 option2
+            {prefix}: option1=val1, option2
+            {prefix}: option1=val1 - option2
+
+        The text is ignored if:
+            * the option declaration is actually a command call,
+            * there is no option declaration in it.
+
+        Args:
+            job: the job to run the handlers on.
+            text: the text to look for option calls in.
+            prefix: the prefix of commands.
+            privileged: run the option handler in privileged mode. Defaults to
+                        False.
+
+        Raises:
+            NotFound: if the option declaration has the right syntax but calls
+                      an unknown option.
+            NotPrivileged: when a privileged option declaration is found
+                           and the method is called with privileged=False.
+
+        """
+        raw = text.strip()
+        if not raw.startswith(prefix):
+            return
+        LOG.debug('Found a potential option: %r', raw)
+        cleaned = re.sub(r'[,.\-/:;|+]', ' ', raw[len(prefix):])
+        match = re.match(r'\s*(?P<keywords>(\s+[\w=]+)+)\s*$', cleaned)
+        if not match:
+            LOG.debug('Ignoring comment. Unknown format')
+            return
+
+        keywords = match.group('keywords').strip().split()
+
+        LOG.debug('checking keywords %s', keywords)
+
+        for idx, kwd in enumerate(keywords):
+            key, *args = kwd.split('=')
+            option = self.dispatch(key)
+            if option is None:
+                raise NotFound(key)
+            if not isinstance(option, Option):
+                if idx == 0:
+                    # It's a command, ignore it
+                    return
+                else:
+                    raise NotFound(key)
+
+            if option.privileged and not privileged:
+                raise NotPrivileged(key)
+
+            # Everything is okay, apply the option
+            option.handler(job, *args)
+
+    def handle_commands(self, job, text, prefix, privileged=False):
+        """Find a command call in given text string, and execute the
+        corresponding handler if any is found.
+
+        An command call can be on the following forms:
+
+            {prefix} command arg1 arg2 ...
+
+        The text is ignored if:
+            * the command call is actually an opotion declaration,
+            * there is no command call in it.
+
+        Args:
+            job: the job to run the handlers on.
+            text: the text to look for command calls in.
+            prefix: the prefix of commands.
+            privileged: run the command handler in privileged mode. Defaults to
+                        False.
+
+        Raises:
+            NotFound: if the command call has the right syntax but calls
+                      an unknown command.
+            NotPrivileged: when a privileged command call is found
+                           and the method is called with privileged=False.
+
+        """
+        if prefix is None:
+            prefix = '@{}'.format(job.settings.robot_username)
+            LOG.debug('looking for prefix: %s', prefix)
+
+        raw = text.strip()
+        if not raw.startswith(prefix):
+            return
+        LOG.debug('Found a potential command: %r', raw)
+        regex = r"%s[\s:]*(?P<command>[A-Za-z_]+[^= ,])(?P<args>.*)$" % prefix
+        match = re.match(regex, raw)
+        if not match:
+            LOG.warning("Command ignored. Unknown format.")
+            return
+
+        key, args = match.group('command'), match.group('args').split()
+        command = self.dispatch(key)
+        if command is None:
+            raise NotFound(key)
+        if not isinstance(command, Command):
+            return
+
+        if command.privileged and not privileged:
+            raise NotPrivileged(key)
+
+        # Execute the command
+        command.handler(job, *args)
---
+++ bert_e/tests/unit/test_reactor.py
@@ -0,0 +1,285 @@
+# Copyright 2016 Scality
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from types import SimpleNamespace
+
+import pytest
+
+from bert_e.reactor import Command, NotFound, NotPrivileged, Option, Reactor
+
+
+# All tests are run on a Reactor subclass to avoid sharing state.
+
+@pytest.fixture
+def reactor_cls():
+    class ReactorTest(Reactor):
+        pass
+
+    return ReactorTest
+
+
+@pytest.fixture
+def job():
+    return SimpleNamespace(settings={})
+
+
+def test_add_option(reactor_cls):
+    """Test options are correctly registered using Reactor.add_option."""
+
+    reactor_cls.add_option('my_option')
+    reactor_cls.add_option('other_option', "help for other_option",
+                           default=False)
+    reactor_cls.add_option('privileged_option', "help for privileged_option",
+                           default=False, privileged=True)
+
+    options = reactor_cls.get_options()
+
+    assert 'my_option' in options
+    assert 'other_option' in options
+    assert 'privileged_option' in options
+
+    assert len(options) == 3
+
+    my_option = options['my_option']
+    other_option = options['other_option']
+    privileged_option = options['privileged_option']
+
+    assert isinstance(my_option, Option)
+    assert my_option.help is None
+    assert not my_option.privileged
+
+    assert isinstance(other_option, Option)
+    assert other_option.help == "help for other_option"
+    assert not other_option.privileged
+
+    assert isinstance(privileged_option, Option)
+    assert privileged_option.help == "help for privileged_option"
+    assert privileged_option.privileged
+
+
+def test_option_decorator(reactor_cls):
+    """Test options are correctly registered using Reactor.option decorator."""
+
+    @reactor_cls.option
+    def my_option(job, *args):
+        """Help for my_option"""
+        pass
+
+    @reactor_cls.option(help_="Help for another_option")
+    def another_option(job, *args):
+        pass
+
+    @reactor_cls.option(default=False)
+    def some_option(job, *args):
+        pass
+
+    @reactor_cls.option(privileged=True)
+    def privileged_option(job, *args):
+        pass
+
+    @reactor_cls.option(key='custom_key')
+    def custom_key_option(job, *args):
+        pass
+
+    options = reactor_cls.get_options()
+
+    assert 'my_option' in options
+    assert 'another_option' in options
+    assert 'some_option' in options
+    assert 'privileged_option' in options
+    assert 'custom_key' in options
+
+    assert len(options) == 5
+
+    assert options['my_option'].handler == my_option
+    assert options['my_option'].help == my_option.__doc__
+    assert options['my_option'].default is None
+    assert not options['my_option'].privileged
+
+    assert options['another_option'].handler == another_option
+    assert options['another_option'].help == "Help for another_option"
+
+    assert options['some_option'].handler == some_option
+    assert options['some_option'].default is False
+
+    assert options['privileged_option'].handler == privileged_option
+    assert options['privileged_option'].privileged
+
+    assert options['custom_key'].handler == custom_key_option
+    assert options['custom_key'].help is None
+
+
+def test_options_behavior(reactor_cls, job):
+    """Test behavior of simple options."""
+
+    reactor_cls.add_option('my_option')
+    reactor_cls.add_option('default_option', default=True)
+    reactor_cls.add_option('privileged_option', privileged=True)
+
+    reactor = reactor_cls()
+
+    reactor.init_settings(job)
+
+    assert job.settings == {
+        'my_option': None,
+        'default_option': True,
+        'privileged_option': None
+    }
+
+    reactor.handle_options(job, '!set my_option', '!set')
+    assert job.settings['my_option'] is True
+
+    with pytest.raises(NotFound):
+        reactor.handle_options(job, '!set blablabla', '!set')
+
+    with pytest.raises(NotPrivileged):
+        reactor.handle_options(job, '!set privileged_option', '!set')
+    assert not job.settings['privileged_option']
+
+    reactor.handle_options(job, '!set privileged_option', '!set', True)
+    assert job.settings['privileged_option'] is True
+
+    job.settings['my_option'] = None
+
+    reactor.handle_options(job, '!set my_option', '!other_prefix')
+    assert job.settings['my_option'] is None
+
+
+def test_add_command(reactor_cls):
+    """Test commands are correctly registered using Reactor.add_command."""
+
+    def my_command(job, *args):
+        pass
+
+    def other_command(job, *args):
+        """
+
+        Help for other_command.
+
+        TBA
+
+        """
+        pass
+
+    def privileged_command(job, *args):
+        pass
+
+    reactor_cls.add_command('my_command', my_command)
+    reactor_cls.add_command('my_command2', my_command, 'help for my_command2')
+    reactor_cls.add_command('other_command', other_command)
+    reactor_cls.add_command('privileged_command', privileged_command,
+                            privileged=True)
+
+    commands = reactor_cls.get_commands()
+
+    assert 'my_command' in commands
+    assert 'my_command2' in commands
+    assert 'other_command' in commands
+    assert 'privileged_command' in commands
+
+    assert isinstance(commands['my_command'], Command)
+    assert commands['my_command'].handler == my_command
+    assert commands['my_command'].help is None
+    assert not commands['my_command'].privileged
+
+    assert commands['my_command2'].handler == my_command
+    assert commands['my_command2'].help == 'help for my_command2'
+
+    assert commands['other_command'].handler == other_command
+    assert commands['other_command'].help == 'Help for other_command. TBA'
+
+    assert commands['privileged_command'].handler == privileged_command
+    assert commands['privileged_command'].privileged
+
+
+def test_command_decorator(reactor_cls):
+    """Test commands are correctly registered using Reactor.command."""
+
+    @reactor_cls.command
+    @reactor_cls.command('my_command2', help_='help for my_command2')
+    def my_command(job, *args):
+        pass
+
+    @reactor_cls.command
+    def other_command(job, *args):
+        """Help for other_command"""
+        pass
+
+    @reactor_cls.command(privileged=True)
+    def privileged_command(job, *args):
+        pass
+
+    commands = reactor_cls.get_commands()
+
+    assert 'my_command' in commands
+    assert 'my_command2' in commands
+    assert 'other_command' in commands
+    assert 'privileged_command' in commands
+
+    assert isinstance(commands['my_command'], Command)
+    assert commands['my_command'].handler == my_command
+    assert commands['my_command'].help is None
+    assert not commands['my_command'].privileged
+
+    assert commands['my_command2'].handler == my_command
+    assert commands['my_command2'].help == 'help for my_command2'
+
+    assert commands['other_command'].handler == other_command
+    assert commands['other_command'].help == 'Help for other_command'
+
+    assert commands['privileged_command'].handler == privileged_command
+    assert commands['privileged_command'].privileged
+
+
+def test_command_behavior(reactor_cls, job):
+    """Test behavior of commands."""
+
+    class CommandCalled(Exception):
+        def __init__(self, job, args):
+            self.job = job
+            self.args = args
+
+    @reactor_cls.command('privileged_cmd', privileged=True)
+    @reactor_cls.command
+    def cmd(job, *args):
+        raise CommandCalled(job, args)
+
+    reactor = reactor_cls()
+
+    with pytest.raises(CommandCalled) as call:
+        reactor.handle_commands(job, '!do cmd', '!do')
+
+    assert call.value.job is job
+    assert call.value.args == ()
+
+    with pytest.raises(CommandCalled) as call:
+        reactor.handle_commands(job, '!do cmd with args', '!do')
+
+    assert call.value.args == ('with', 'args')
+
+    with pytest.raises(NotFound):
+        reactor.handle_commands(job, '!do command', '!do')
+
+    try:
+        reactor.handle_commands(job, '!do cmd', '!other_prefix')
+    except CommandCalled:
+        assert False, "Command shouldn't have been called"
+
+    with pytest.raises(NotPrivileged):
+        reactor.handle_commands(job, '!do privileged_cmd', '!do')
+
+    with pytest.raises(CommandCalled) as call:
+        reactor.handle_commands(job, '!do privileged_cmd arg', '!do',
+                                privileged=True)
+
+    assert call.value.args == ('arg',)
