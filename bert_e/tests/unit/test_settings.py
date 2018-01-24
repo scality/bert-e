e5462c93a68f20bb80ae51b6e2c895f9469575b5
Thomas Carmet <thomas.carmet@scality.com>
2018-01-24 14:36:06 +0100

    BERTE-418 - Adding unit test for log filter

---
+++ bert_e/tests/unit/test_settings.py
@@ -0,0 +1,21 @@
+
+import pytest
+import logging
+import os.path
+from bert_e.settings import BertEContextFilter, setup_settings
+
+
+@pytest.fixture
+def settings():
+    """Simple settings fixture."""
+    return setup_settings(os.path.abspath('settings.sample.yml'))
+
+
+def test_log_filter(settings):
+    """Test a simple instanciation of BertEContextFilter."""
+    logging.basicConfig(format='%(instance)s - %(level)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    log_filter = BertEContextFilter(settings)
+    logger.addFilter(log_filter)
+    logger.info('hello')
+    assert len(logger.filters) is 1
