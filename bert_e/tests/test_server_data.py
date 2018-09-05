bea58e55b397d09a9ba1b7208bb4e513ef24e7d2
Sylvain Killian <sylvain.killian@scality.com>
2018-09-05 08:34:36 +0200

    BERTE-366 update copyright date

--- bert_e/tests/test_server_data.py
+++ bert_e/tests/test_server_data.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-# Copyright 2016 Scality
+# Copyright 2016-2018 Scality
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
