2e31649b0ef1b3273df8a52db1d537cd02b51f3f
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2020-06-16 18:06:58 +0200

    revert tests change

--- bert_e/tests/test_bert_e.py
+++ bert_e/tests/test_bert_e.py
@@ -1155,7 +1155,6 @@ admins:
                             'project/invalid',
                             'feature/invalid',
                             'hotfix/customer',
-                            'hotfix/6.6.6.1',
                             'dependabot/npm_and_yarn/ui/lodash-4.17.13']:
             create_branch(self.gitrepo, destination, file_=True,
                           from_branch='development/4.3')
