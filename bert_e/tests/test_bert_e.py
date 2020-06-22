f4cc7eaa108bf92bcfa91af3db428434d497959e
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2020-06-22 15:44:38 +0200

    please linter

--- bert_e/tests/test_bert_e.py
+++ bert_e/tests/test_bert_e.py
@@ -433,15 +433,15 @@ class QuickTest(unittest.TestCase):
             8: {'name': 'development/10.0', 'ignore': True}
         })
         tags = ['4.3.16', '4.3.17', '4.3.18_rc1', '5.1.3', '5.1.4_rc1',
-                '6.6.6', '10.0.3.1' ]
+                '6.6.6', '10.0.3.1']
         fixver = ['6.6.6.1']
         self.finalize_cascade(branches, tags, destination, fixver)
         tags = ['4.3.16', '4.3.17', '4.3.18_rc1', '5.1.3', '5.1.4_rc1',
-                '6.6.6.0', '10.0.3.1' ]
+                '6.6.6.0', '10.0.3.1']
         fixver = ['6.6.6.1']
         self.finalize_cascade(branches, tags, destination, fixver)
         tags = ['4.3.16', '4.3.17', '4.3.18_rc1', '5.1.3', '5.1.4_rc1',
-                '6.6.6.1', '10.0.3.1' ]
+                '6.6.6.1', '10.0.3.1']
         fixver = ['6.6.6.2']
         self.finalize_cascade(branches, tags, destination, fixver)
 
